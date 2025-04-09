c6ed5584a96da9093b00e1b449d8f80e9a8cd652
Mudasir Rehman <mudassir.rehman@insigniabiz.com>
2025-04-09 20:34:07 +0500

    test: add GetVoucherCode and RenderTemplateAsync success unit tests

---
+++ EmailSenderProgram/Tests/TemplateManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using EmailSenderProgram.Infrastructure.Managers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmailSenderProgram.Tests
+{
+    public class TemplateManagerTests
+    {
+        [Fact]
+        public async Task RenderTemplateAsync_ShouldRenderTemplate_WithVariables()
+        {
+            
+            var manager = new TemplateManager("DummyPath"); 
+            var template = "Hello {{ name }}!";
+            var variables = new Dictionary<string, object>
+                            {
+                            { "name", "Alice" }
+                            };
+           
+            var result = await manager.RenderTemplateAsync(template, variables);
+          
+            Assert.Equal("Hello Alice!", result);
+        }
+    }
+}
---
+++ EmailSenderProgram/Tests/VoucherManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using EmailSenderProgram.Infrastructure.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmailSenderProgram.Tests
+{
+    public class VoucherManagerTests
+    {
+        [Fact]
+        public void GetVoucherCode_ShouldReturnExpectedCode()
+        {
+            
+            var voucherManager = new VoucherManager();
+            var result = voucherManager.GetVoucherCode();
+            Assert.Equal("EOComebackDiscount", result);
+        }
+    }
+}
